af7219dfaf507feb60a4f2f87f98491088804918
Ciprian Ursulean <ciprian.ursulean5@gmail.com>
2017-01-01 18:21:54 +0200

    Solved a navigation between tabs bug

--- scripts/design_scripts/operatii_matematice_layout.js
+++ scripts/design_scripts/operatii_matematice_layout.js
@@ -4,7 +4,6 @@ $(document).ready(function(){
     $("#menu, #conversie_info").css("background", "#FF4081");
     $(".conversie_tab, #conversie_info").show();
     $("header, #alte_informatii").css("background", "#3F51B5");
-    $("#alte_informatii h2, .card a").css("color", "#FF4081");
     $("header nav").css("background", "#303F9F");
     $("header nav ul li a, .card .content").css("color", "#C5CAE9");
 
@@ -15,7 +14,6 @@ $(document).ready(function(){
         $("#adunare_tab, #adunare_info, #scadere_tab, #scadere_info, #inmultire_tab, #inmultire_info, #impartire_tab, #impartire_info, #radical_tab, #radical_info, #ridicare_putere_tab, #putere_info, #radical_tab, #radical_info").hide();
         $("header, #alte_informatii").css("background", "#3F51B5");
         $("header h1").html("Baze de numeratie");
-        $("#alte_informatii h2, .card a").css("color", "#FF4081");
         $("header nav, #conversie_info").css("background", "#303F9F");
         $("header nav ul li a, .card .content").css("color", "#C5CAE9");
         $("#menu, #conversie_info").css("background", "#FF4081");
@@ -24,10 +22,9 @@ $(document).ready(function(){
     // adunare tab event-listener (click)
    $("#plus").click(function(){
        $("div#adunare_tab, #adunare_info").show();
-       $("#inmultire_tab, #inmultire_info, #impartire_tab, #impartire_info, #radical_tab, #radical_info, #setari_tab, .conversie_tab, #conversie_info, #ridicare_putere_tabm, #putere_info").hide();
+       $("#inmultire_tab, #inmultire_info, #impartire_tab, #impartire_info, #radical_tab, #radical_info, #setari_tab, .conversie_tab, #conversie_info, #ridicare_putere_tab, #putere_info, #scadere_tab, #scadere_info").hide();
        $("header, #alte_informatii").css("background", "#FFC107");
        $("header h1").html("Adunarea numerelor");
-       $("#alte_informatii h2, .card a").css("color", "#607D8B");
        $("header nav, #adunare_info").css("background", "#FFA000");
        $("header nav ul li a, .card .content").css("color", "#FFECB3");
        $("#menu, #adunare_info").css("background", "#607D8B");
@@ -36,10 +33,9 @@ $(document).ready(function(){
     // scadere tab event-listener (click)
    $("#minus").click(function(){
        $("div#scadere_tab, #scadere_info").show();
-       $("#adunare_tab, #adunare_info, #inmultire_tab, #inmultire_info, #impartire_tab, #impartire_info, #radical_tab, #radical_info, #setari_tab, .conversie_tab, #conversie_info ,#ridicare_putere_tabm, #putere_info").hide();
+       $("#adunare_tab, #adunare_info, #inmultire_tab, #inmultire_info, #impartire_tab, #impartire_info, #radical_tab, #radical_info, #setari_tab, .conversie_tab, #conversie_info ,#ridicare_putere_tab, #putere_info").hide();
        $("header, #alte_informatii").css("background", "#009688");
        $("header h1").html("Scaderea numerelor");
-       $("#alte_informatii h2, .card a").css("color", "#FF4081");
        $("header nav, #scadere_info").css("background", "#00796B");
        $("header nav ul li a, .card .content").css("color", "#B2DFDB");
        $("#menu, #scadere_info").css("background", "#FF4081");
@@ -48,10 +44,9 @@ $(document).ready(function(){
     // inmultire tab event-listener (click)
    $("#ori").click(function(){
        $("div#inmultire_tab, #inmultire_info").show();
-       $("#adunare_tab, #adunare_info, #scadere_tab, #scadere_info, #impartire_tab, #impartire_info, #radical_tab, #radical_info, #setari_tab, .conversie_tab, #conversie_info, #ridicare_putere_tabm, #putere_info").hide();
+       $("#adunare_tab, #adunare_info, #scadere_tab, #scadere_info, #impartire_tab, #impartire_info, #radical_tab, #radical_info, #setari_tab, .conversie_tab, #conversie_info, #ridicare_putere_tab, #putere_info").hide();
        $("header, #alte_informatii").css("background", "#03A9F4");
        $("header h1").html("Inmultirea numerelor");
-       $("#alte_informatii h2, .card a").css("color", "#FFC107");
        $("header nav, #inmultire_info").css("background", "#0288D1");
        $("header nav ul li a, .card .content").css("color", "#B3E5FC");
        $("#menu, #inmultire_info").css("background", "#FFC107");
@@ -60,10 +55,9 @@ $(document).ready(function(){
     // impartire tab event-listener (click)
    $("#divide").click(function(){
        $("div#impartire_tab, #impartire_info").show();
-       $("#adunare_tab, #adunare_info, #scadere_tab, #scadere_info, #inmultire_tab, #inmultire_info, #radical_tab, #radical_info, #setari_tab, .conversie_tab, #conversie_info, #ridicare_putere_tabm, #putere_info").hide();
+       $("#adunare_tab, #adunare_info, #scadere_tab, #scadere_info, #inmultire_tab, #inmultire_info, #radical_tab, #radical_info, #setari_tab, .conversie_tab, #conversie_info, #ridicare_putere_tab, #putere_info").hide();
        $("header, #alte_informatii").css("background", "#F44336");
        $("header h1").html("Impartirea numerelor");
-       $("#alte_informatii h2, .card a").css("color", "#9E9E9E");
        $("header nav, #impartire_info").css("background", "#D32F2F");
        $("header nav ul li a, .card .content").css("color", "#FFCDD2");
        $("#menu, #impartire_info").css("background", "#9E9E9E");
@@ -72,10 +66,9 @@ $(document).ready(function(){
     // radicali tab event-listener (click)
    $("#radical").click(function(){
        $("div#radical_tab, #radical_info").show();
-       $("#adunare_tab,#adunare_info,  #scadere_tab, #scadere_info, #inmultire_tab, #inmultire_info, #impartire_tab, #impartire_info, #ridicare_putere_tab, #putere_info, #setari_tab, .conversie_tab, #conversie_info, #ridicare_putere_tabm, #putere_info").hide();
+       $("#adunare_tab, #adunare_info,  #scadere_tab, #scadere_info, #inmultire_tab, #inmultire_info, #impartire_tab, #impartire_info, #ridicare_putere_tab, #putere_info, #setari_tab, .conversie_tab, #conversie_info, #ridicare_putere_tabm, #putere_info").hide();
        $("header, #alte_informatii").css("background", "#607D8B");
        $("header h1").html("Radicali ai numerelor");
-       $("#alte_informatii h2, .card a").css("color", "#FF5722");
        $("header nav, #radical_info").css("background", "#455A64");
        $("header nav ul li a, .card .content").css("color", "#CFD8DC");
        $("#menu, #radical_info").css("background", "#FF5722");
@@ -87,7 +80,6 @@ $(document).ready(function(){
        $("#adunare_tab, #adunare_info, #scadere_tab, #scadere_info, #inmultire_tab, #inmultire_info, #impartire_tab, #impartire_info, #radical_tab, #radical_info, #setari_tab, .conversie_tab, #conversie_info").hide();
        $("header, #alte_informatii").css("background", "#00BCD4");
        $("header h1").html("Puteri ale numerelor");
-       $("#alte_informatii h2, .card a").css("color", "#FF9800");
        $("header nav, #putere_info").css("background", "#0097A7");
        $("header nav ul li a, .card .content").css("color", "#B2EBF2");
        $("#menu, #putere_info").css("background", "#FF9800");
@@ -100,7 +92,6 @@ $(document).ready(function(){
        $("#menu").css("background", "#68EFAD");
        $("header, #alte_informatii").css("background", "#9C27B0");
        $("header h1").html("Setari");
-       $("#alte_informatii h2, .card a").css("color", "#68EFAD");
        $("header nav").css("background", "#7B1FA2");
        $("header nav ul li a, .card .content").css("color", "#E1BEE7");
    });
