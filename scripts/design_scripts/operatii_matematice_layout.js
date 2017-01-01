27ed05260acc8c4a44d634b0b6b9cfd802d20500
Ciprian Ursulean <ciprian.ursulean5@gmail.com>
2017-01-01 17:04:53 +0200

    Adding navbar and improving design themes

--- scripts/design_scripts/operatii_matematice_layout.js
+++ scripts/design_scripts/operatii_matematice_layout.js
@@ -1,96 +1,115 @@
 $(document).ready(function(){
-    $("header h1").fadeIn("slow");
+    // initial state of the app
+    $("header h1").html("Math Beast").fadeIn("slow");
     $(".conversie_tab, #conversie_info").show();
-    $("header, #alte_informatii").css("background", "#1976D2");
-    $("#menu").css("background", "#FFC107");
-    $("header h1").html("Math Beast");
-    $("#alte_informatii h2, .card a").css("color", "#FFC107");
+    $("header, #alte_informatii").css("background", "#3F51B5");
+    $("#menu").css("background", "#FF4081");
+    $("#alte_informatii h2, .card a").css("color", "#FF4081");
     $("#conversie_info").css("background", "#9C27B0");
+    $("header nav").css("background", "#303F9F");
+    $("header nav ul li a, .card .content").css("color", "#C5CAE9");
 
+
+    // conversie tab event-listener (click)
     $("#conversie").click(function(){
         $("div.conversie_tab, #conversie_info").show();
         $("#adunare_tab, #adunare_info, #scadere_tab, #scadere_info, #inmultire_tab, #inmultire_info, #impartire_tab, #impartire_info, #radical_tab, #radical_info, #ridicare_putere_tab, #putere_info, #radical_tab, #radical_info").hide();
-        $(".adunare, .scadere, .inmultire, .impartire, .radical, .putere").css("background", "none");
-        $("#menu").css("background", "#FFC107");
-        $("header, #alte_informatii").css("background", "#1976D2");
+        $("#menu").css("background", "#FF4081");
+        $("header, #alte_informatii").css("background", "#3F51B5");
         $("header h1").html("Baze de numeratie");
-        $("#alte_informatii h2, .card a").css("color", "#FFC107");
+        $("#alte_informatii h2, .card a").css("color", "#FF4081");
         $("#conversie_info").css("background", "#9C27B0");
+        $("header nav").css("background", "#303F9F");
+        $("header nav ul li a, .card .content").css("color", "#C5CAE9");
     });
 
+    // adunare tab event-listener (click)
    $("#plus").click(function(){
        $("div#adunare_tab, #adunare_info").show();
-       $("#scadere_tab, #scadere_info, #impartire_tab, #impartire_info, #inmultire_tab, #inmultire_info, #radical_tab, #radical_info, #setari_tab, .conversie_tab, #conversie_info").hide();
-       $(".conversie, .scadere, .inmultire, .impartire, .radical, .putere, .setari").css("background", "none");
+       $("#inmultire_tab, #inmultire_info, #impartire_tab, #impartire_info, #radical_tab, #radical_info, #setari_tab, .conversie_tab, #conversie_info, #ridicare_putere_tabm, #putere_info").hide();
        $("#menu").css("background", "#607D8B");
        $("header, #alte_informatii").css("background", "#FFC107");
        $("header h1").html("Adunarea numerelor");
        $("#alte_informatii h2, .card a").css("color", "#607D8B");
        $("#adunare_info").css("background", "#0050ef");
+       $("header nav").css("background", "#FFA000");
+       $("header nav ul li a, .card .content").css("color", "#FFECB3");
    });
 
+    // scadere tab event-listener (click)
    $("#minus").click(function(){
        $("div#scadere_tab, #scadere_info").show();
-       $("#adunare_tab, #adunare_info, #inmultire_tab, #inmultire_info, #impartire_tab, #impartire_info, #radical_tab, #radical_info, #setari_tab, .conversie_tab, #conversie_info").hide();
-       $(".conversie, .adunare, .inmultire, .impartire, .radical, .putere, .setari").css("background", "none");
-      $("#menu").css("background", "#d80073");
-      $("header, #alte_informatii").css("background", "#008a00");
-      $("header h1").html("Scaderea numerelor");
-      $("#alte_informatii h2, .card a").css("color", "#d80073");
-      $("#scadere_info").css("background", "#34495e");
+       $("#adunare_tab, #adunare_info, #inmultire_tab, #inmultire_info, #impartire_tab, #impartire_info, #radical_tab, #radical_info, #setari_tab, .conversie_tab, #conversie_info ,#ridicare_putere_tabm, #putere_info").hide();
+       $("#menu").css("background", "#FF4081");
+       $("header, #alte_informatii").css("background", "#009688");
+       $("header h1").html("Scaderea numerelor");
+       $("#alte_informatii h2, .card a").css("color", "#FF4081");
+       $("#scadere_info").css("background", "#34495e");
+       $("header nav").css("background", "#00796B");
+       $("header nav ul li a, .card .content").css("color", "#B2DFDB");
    });
 
+    // inmultire tab event-listener (click)
    $("#ori").click(function(){
        $("div#inmultire_tab, #inmultire_info").show();
-      $("#adunare_tab, #adunare_info, #scadere_tab, #scadere_info, #impartire_tab, #impartire_info, #radical_tab, #radical_info, #setari_tab, .conversie_tab, #conversie_info").hide();
-      $(".conversie, .adunare, .scadere, .impartire, .radical, .putere, .setari").css("background", "none");
-      $("#menu").css("background", "#a20025");
-      $("header, #alte_informatii").css("background", "#00aba9");
-      $("header h1").html("Inmultirea numerelor");
-      $("#alte_informatii h2, .card a").css("color", "#a20025");
+       $("#adunare_tab, #adunare_info, #scadere_tab, #scadere_info, #impartire_tab, #impartire_info, #radical_tab, #radical_info, #setari_tab, .conversie_tab, #conversie_info, #ridicare_putere_tabm, #putere_info").hide();
+       $("#menu").css("background", "#FFC107");
+       $("header, #alte_informatii").css("background", "#03A9F4");
+       $("header h1").html("Inmultirea numerelor");
+       $("#alte_informatii h2, .card a").css("color", "#FFC107");
        $("#inmultire_info").css("background", "#2196F3");
+       $("header nav").css("background", "#0288D1");
+       $("header nav ul li a, .card .content").css("color", "#B3E5FC");
    });
 
+    // impartire tab event-listener (click)
    $("#divide").click(function(){
        $("div#impartire_tab, #impartire_info").show();
-      $("#adunare_tab, #adunare_info, #scadere_tab, #scadere_info, #inmultire_tab, #inmultire_info, #radical_tab, #radical_info, #setari_tab, .conversie_tab, #conversie_info").hide();
-      $(".conversie, .adunare, .scadere, .inmultire, .radical, .putere, .setari").css("background", "none");
-      $("#menu").css("background", "#e51400");
-      $("header, #alte_informatii").css("background", "#1ba1e2");
-      $("header h1").html("Impartirea numerelor");
-      $("#alte_informatii h2, .card a").css("color", "#e51400");
-      $("#impartire_info").css("background", "#2ecc71");
+       $("#adunare_tab, #adunare_info, #scadere_tab, #scadere_info, #inmultire_tab, #inmultire_info, #radical_tab, #radical_info, #setari_tab, .conversie_tab, #conversie_info, #ridicare_putere_tabm, #putere_info").hide();
+       $("#menu").css("background", "#9E9E9E");
+       $("header, #alte_informatii").css("background", "#F44336");
+       $("header h1").html("Impartirea numerelor");
+       $("#alte_informatii h2, .card a").css("color", "#9E9E9E");
+       $("#impartire_info").css("background", "#2ecc71");
+       $("header nav").css("background", "#D32F2F");
+       $("header nav ul li a, .card .content").css("color", "#FFCDD2");
    });
 
+    // radicali tab event-listener (click)
    $("#radical").click(function(){
        $("div#radical_tab, #radical_info").show();
-      $("#adunare_tab,#adunare_info,  #scadere_tab, #scadere_info, #inmultire_tab, #inmultire_info, #impartire_tab, #impartire_info, #ridicare_putere_tab, #putere_info, #setari_tab, .conversie_tab, #conversie_info").hide();
-      $(".conversie, .adunare, .scadere, .inmultire, .impartire, .putere, .setari").css("background", "none");
-      $("#menu").css("background", "#FF4081");
-      $("header, #alte_informatii").css("background", "#5D4037");
-      $("header h1").html("Radicali ai numerelor");
-      $("#alte_informatii h2, .card a").css("color", "#FF4081");
-      $("#radical_info").css("background", "#FFB300");
+       $("#adunare_tab,#adunare_info,  #scadere_tab, #scadere_info, #inmultire_tab, #inmultire_info, #impartire_tab, #impartire_info, #ridicare_putere_tab, #putere_info, #setari_tab, .conversie_tab, #conversie_info, #ridicare_putere_tabm, #putere_info").hide();
+       $("#menu").css("background", "#FF5722");
+       $("header, #alte_informatii").css("background", "#607D8B");
+       $("header h1").html("Radicali ai numerelor");
+       $("#alte_informatii h2, .card a").css("color", "#FF5722");
+       $("#radical_info").css("background", "#FFB300");
+       $("header nav").css("background", "#455A64");
+       $("header nav ul li a, .card .content").css("color", "#CFD8DC");
    });
 
+    // puteri tab event-listener (click)
    $("#putere").click(function(){
        $("div#ridicare_putere_tab, #putere_info").show();
-      $("#adunare_tab, #adunare_info, #scadere_tab, #scadere_info, #inmultire_tab, #inmultire_info, #impartire_tab, #impartire_info, #radical_tab, #radical_info, #setari_tab, .conversie_tab, #conversie_info").hide();
-      $(".conversie, .adunare, .scadere, .inmultire, .impartire, .radical, .setari").css("background", "none");
-      $("#menu").css("background", "#FF5722");
-      $("header, #alte_informatii").css("background", "#6a00ff");
-      $("header h1").html("Puteri ale numerelor");
-      $("#alte_informatii h2, .card a").css("color", "#FF5722");
-      $("#putere_info").css("background", "#00796B");
+       $("#adunare_tab, #adunare_info, #scadere_tab, #scadere_info, #inmultire_tab, #inmultire_info, #impartire_tab, #impartire_info, #radical_tab, #radical_info, #setari_tab, .conversie_tab, #conversie_info").hide();
+       $("#menu").css("background", "#FF9800");
+       $("header, #alte_informatii").css("background", "#00BCD4");
+       $("header h1").html("Puteri ale numerelor");
+       $("#alte_informatii h2, .card a").css("color", "#FF9800");
+       $("#putere_info").css("background", "#00796B");
+       $("header nav").css("background", "#0097A7");
+       $("header nav ul li a, .card .content").css("color", "#B2EBF2");
    });
 
+    // setari tab event-listener (click)
    $("#setari").click(function(){
-      $("div#setari_tab").show();
-      $("#adunare_tab, #scadere_tab, #inmultire_tab, #impartire_tab, #radical_tab, #ridicare_putere_tab, .conversie_tab").hide();
-      $(".conversie, .adunare, .scadere, .inmultire, .impartire, .radical, .putere").css("background", "none");
-      $("#menu").css("background", "#00FF00");
-      $("header, #alte_informatii").css("background", "#000080");
-      $("header h1").html("Setari");
-      $("#alte_informatii h2, .card a").css("color", "#00FF00");
+       $("div#setari_tab").show();
+       $("#adunare_tab, #scadere_tab, #inmultire_tab, #impartire_tab, #radical_tab, #ridicare_putere_tab, .conversie_tab, #adunare_info, #scadere_info, #inmultire_info, #impartire_info, #conversie_info, #putere_info, #radical_info, #radical_info").hide();
+       $("#menu").css("background", "#68EFAD");
+       $("header, #alte_informatii").css("background", "#9C27B0");
+       $("header h1").html("Setari");
+       $("#alte_informatii h2, .card a").css("color", "#68EFAD");
+       $("header nav").css("background", "#7B1FA2");
+       $("header nav ul li a, .card .content").css("color", "#E1BEE7");
    });
 });
---
+++ scss/Themes.md
@@ -0,0 +1,49 @@
+# headers, menu, information areas themes only
+
+# Conversii theme
+primary: #3F51B5;
+primary-dark: #303F9F;
+primary-light: #C5CAE9;
+accent: #FF4081;
+
+# Adunare theme
+primary: #FFC107;
+primary-dark: #FFA000;
+primary-light: #FFECB3;
+accent: #607D8B;
+
+# Scadere theme
+primary: #009688;
+primary-dark: #00796B;
+primary-light: #B2DFDB;
+accent: #FF4081;
+
+# Inmultire theme
+primary: #03A9F4;
+primary-dark: #0288D1;
+primary-light: #B3E5FC;
+accent: #FFC107;
+
+# Impartire theme
+primary: #F44336;
+primary-dark: #D32F2F;
+primary-light: #FFCDD2;
+accent: #9E9E9E;
+
+# Puteri theme
+primary: #00BCD4;
+primary-dark: #0097A7;
+primary-light: #B2EBF2;
+accent: #FF9800;
+
+# Radicali theme
+primary: #607D8B;
+primary-dark: #455A64;
+primary-light: #CFD8DC;
+accent: #FF5722;
+
+# Setari theme
+primary: #9C27B0;
+primary-dark: #7B1FA2;
+primary-light: #E1BEE7;
+accent: #68EFAD;
