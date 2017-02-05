9fa789258d9a3622c6e725689064b8152ea7fdc5
Ciprian Ursulean <Ciprian Ursulean>
2017-02-05 03:41:33 -0800

    Some refactorings

--- scripts/design_scripts/setari.js
+++ scripts/design_scripts/setari.js
@@ -1,14 +1,27 @@
 $(document).ready(function(){
-    $("body, input, #adunare_tab, #scadere_tab, #inmultire_tab, #impartire_tab, #ridicare_putere_tab, #radical_tab, #setari_tab, .conversie_tab, .solutions").css("background" , "white");
-    $(".presol_add, .presol_sub, .presol_inm, .presol_imp, .presol_pow, .presol_rad, .presol, #rezultatulAdunarii, #rezultatulScaderii, #rezultatulInmultirii, #rezultatulImpartirii, #rezultatulRadicalului, #rezultatulRidicarii, .b, input").css("color" , "#3d4754");
+    function default_theme() {
+        $("body, input, #adunare_tab, #scadere_tab, #inmultire_tab, #impartire_tab, #ridicare_putere_tab, #radical_tab, #setari_tab, .conversie_tab, .solutions").css("background" , "white");
+        $(".presol_add, .presol_sub, .presol_inm, .presol_imp, .presol_pow, .presol_rad, .presol, #rezultatulAdunarii, #rezultatulScaderii, #rezultatulInmultirii, #rezultatulImpartirii, #rezultatulRadicalului, #rezultatulRidicarii, .b, input").css("color" , "#3d4754");
+    }
 
-});
-   $("#light").click(function(){
-       $("body, input, #adunare_tab, #scadere_tab, #inmultire_tab, #impartire_tab, #ridicare_putere_tab, #radical_tab, #setari_tab, .conversie_tab, .solutions").css("background" , "white");
-       $(".presol_add, .presol_sub, .presol_inm, .presol_imp, .presol_pow, .presol_rad, .presol, #rezultatulAdunarii, #rezultatulScaderii, #rezultatulInmultirii, #rezultatulImpartirii, #rezultatulRadicalului, #rezultatulRidicarii, .b, input").css("color" , "#3d4754");
-   });
+    function light_theme() {
+        $("body, input, #adunare_tab, #scadere_tab, #inmultire_tab, #impartire_tab, #ridicare_putere_tab, #radical_tab, #setari_tab, .conversie_tab, .solutions").css("background" , "white");
+        $(".presol_add, .presol_sub, .presol_inm, .presol_imp, .presol_pow, .presol_rad, .presol, #rezultatulAdunarii, #rezultatulScaderii, #rezultatulInmultirii, #rezultatulImpartirii, #rezultatulRadicalului, #rezultatulRidicarii, .b, input").css("color" , "#3d4754");
+    }
 
-   $("#dark").click(function(){
-    $("body, input, #adunare_tab, #scadere_tab, #inmultire_tab, #impartire_tab, #ridicare_putere_tab, #radical_tab, #setari_tab, .conversie_tab, .solutions").css("background" , "#212121");
-    $(".presol_add, .presol_sub, .presol_inm, .presol_imp, .presol_pow, .presol_rad, .presol, #rezultatulAdunarii, #rezultatulScaderii, #rezultatulInmultirii, #rezultatulImpartirii, #rezultatulRadicalului, #rezultatulRidicarii, .b, input").css("color" , "white");
+    function dark_theme() {
+        $("body, input, #adunare_tab, #scadere_tab, #inmultire_tab, #impartire_tab, #ridicare_putere_tab, #radical_tab, #setari_tab, .conversie_tab, .solutions").css("background" , "#212121");
+        $(".presol_add, .presol_sub, .presol_inm, .presol_imp, .presol_pow, .presol_rad, .presol, #rezultatulAdunarii, #rezultatulScaderii, #rezultatulInmultirii, #rezultatulImpartirii, #rezultatulRadicalului, #rezultatulRidicarii, .b, input").css("color" , "white");
+    }
+
+    default_theme();
+
+    $("#light").click(function(){
+        light_theme();
+    });
+
+    $("#dark").click(function(){
+        dark_theme();
+    });
+
 });
